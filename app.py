f064477ea2666d4184adc6920b8da121e3c9495a
mikecarti <mikecartibeats@gmail.com>
2023-08-15 06:48:31 -0400

    retries for anti spam fix

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 BASE_URL = "http://0.0.0.0:8000"
 ADD_MESSAGE_URL = f"{BASE_URL}/add_message"
-answer_message_url = f"{BASE_URL}/answer_message"
+ANSWER_MESSAGE_URL = f"{BASE_URL}/answer_message"
+CLEAR_MEMORY_URL = f"{BASE_URL}/clear_memory/" + "{user_id}"
+
+LIMIT_EXCEEDED_CODE = 429
 
 
 @app.route("/")
@@ -21,9 +24,13 @@ def index():
 def process_text():
     user_input = request.form["user_input"]
     user_id = 228228
-    send_message_to_processing(text=user_input, user_id=user_id)
-    sleep(1)
-    response = receive_answer(user_id)
+
+    if user_input == "/clear":
+        response = clear_memory(user_id=user_id)
+    else:
+        send_message_to_processing(text=user_input, user_id=user_id)
+        sleep(0.1)
+        response = receive_answer(user_id)
     answer_text = response.get("text")
     return render_template("index.html", output=answer_text)
 
@@ -46,7 +53,13 @@ def send_message_to_processing(text: str, user_id: int) -> None:
 
 
 def receive_answer(user_id: int) -> dict:
-    response = requests.post(answer_message_url, json={"user_id": user_id})
+    while True:
+        response = requests.post(ANSWER_MESSAGE_URL, json={"user_id": user_id})
+        wait_btw_retries_seconds = 1
+        sleep(wait_btw_retries_seconds)
+        logger.warning(f"Anti-Spam limit exceeded. Retrying in {wait_btw_retries_seconds} seconds...")
+        if response.status_code != LIMIT_EXCEEDED_CODE:
+            break
 
     if response.status_code == 200:
         answer = response.json().get("text")
@@ -54,7 +67,13 @@ def receive_answer(user_id: int) -> dict:
         return response.json()
     else:
         logger.debug("Failed to get answer")
-        return {}
+        return {"status_code": response.status_code, "detail": response.json().get("detail")}
+
+
+def clear_memory(user_id):
+    url = CLEAR_MEMORY_URL.format(user_id=user_id)
+    response = requests.post(url)
+    return response.json()
 
 
 if __name__ == "__main__":
