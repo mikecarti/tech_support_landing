12238d608dd2195d4b5321b88c7972c0013a3e42
NeuroSquare <NeuroSquareDev@gmail.com>
2023-08-30 17:33:27 +0300

    stable prod

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def clear_memory(user_id):
     return response.json()
 
 def main():
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
     # app.run(debug=True)
 
 if __name__ == "__main__":
