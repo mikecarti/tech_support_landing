b5bb437580776843ac317a10bd011186ae6b0e83
svetadomoi <heavensfeel@yandex.ru>
2023-09-04 22:27:53 +0300

    skip intro option

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -11,6 +11,7 @@ const giga_chat_message_container = document.getElementById("giga-chat-messages"
 const senders = ["user", "llm"]
 const giga_senders = ["giga-user", "giga-llm"]
 window.cancelWelcome = false;
+const skip_intro = true;
 
 export async function sleep(ms) {
     return new Promise(async resolve => await setTimeout(resolve, ms));
@@ -53,29 +54,32 @@ async function welcome_chat(){
 giga_chat.classList.toggle("active")
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const gif = document.createElement("img");
-  const dialog_number = Math.floor(Math.random() * (dialogues.length - 0 + 1)) + 0;
-  chat_container.style.bottom = "0px";
-  var sender_id = 1;
-  await sleep(2000)
-  for (let i = 0; i < dialogues[dialog_number].length; i++) {
-    drawMessage(senders[sender_id%2], dialogues[dialog_number][i], welcomeMessageContainer);
-    sender_id += 1;
-    scrollToBottom();
-    await sleep(1500);
-  }
-  await fetch("/static/images/hammer.gif")
-  .then(async response => await response.blob())
-  .then(blob => {
-    gif.src = URL.createObjectURL(blob);
-    //document.getElementById("sliders").appendChild(gif);
-  });
+  if (!skip_intro) {
+    const gif = document.createElement("img");
+    const dialog_number = Math.floor(Math.random() * (dialogues.length - 0 + 1)) + 0;
+    chat_container.style.bottom = "0px";
+    var sender_id = 1;
+    await sleep(2000)
+    for (let i = 0; i < dialogues[dialog_number].length; i++) {
+      drawMessage(senders[sender_id%2], dialogues[dialog_number][i], welcomeMessageContainer);
+      sender_id += 1;
+      scrollToBottom();
+      await sleep(1500);
+    }
+    await fetch("/static/images/hammer.gif")
+    .then(async response => await response.blob())
+    .then(blob => {
+      gif.src = URL.createObjectURL(blob);
+      //document.getElementById("sliders").appendChild(gif);
+    });
   
   await sleep(1300)
   chat_container.style.bottom = "-600px";
   await sleep(500)
   gif.style.display = "none"
   await sleep(1500)
+  }
+  
   drawMessage("giga-llm", "Простите моего коллегу, он устарел.", giga_chat_message_container);
   await sleep(1500)
   drawMessage("giga-llm", "Чем могу быть полезен?", giga_chat_message_container);
