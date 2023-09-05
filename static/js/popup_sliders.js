bbf8763352406422e048c64ac2fc54534369eafc
svetadomoi <heavensfeel@yandex.ru>
2023-09-05 20:52:37 +0300

    Ползунки на мобиле теперь выдвигаются слева

--- static/js/popup_sliders.js
+++ static/js/popup_sliders.js
@@ -3,15 +3,15 @@ const sliders_popup = document.getElementById('sliders-container');
 const sliders = document.getElementById('sliders');
 
 let sliders_up = false;
-let sliders_height = sliders.offsetHeight;
-sliders_popup.style.bottom = `-${sliders_height}px`;
+let sliders_width = sliders.offsetWidth;
+sliders_popup.style.left = `-${sliders_width}px`;
 
 sliders_popup_button.addEventListener('click', () => {
     
     if (sliders_up) {
-        sliders_popup.style.bottom = `-${sliders.offsetHeight}px`;
+        sliders_popup.style.left = `-${sliders.offsetWidth}px`;
     } else {
-        sliders_popup.style.bottom = '0';
+        sliders_popup.style.left = '0';
     }
     
     sliders_up = !sliders_up;
