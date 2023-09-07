4941acd22422d18979a5cb414bf373983789948f
mikecarti <mikecartibeats@gmail.com>
2023-09-07 11:08:40 -0400

    block messaging while user did not receive answer

--- static/js/chat_functionality.js
+++ static/js/chat_functionality.js
@@ -44,20 +44,35 @@ export async function sendMessageAndGetResponseWelcome(message, chat_messages_co
 
 }
 
+let isAllowedToSend = true; // Изначально разрешаем отправку сообщений
+
 export async function sendMessageAndGetResponse(message, chat_messages_container, nodeSliders, endpoint, senders) {
+    const sendButton = document.getElementById('giga-chat-send-button'); // Получаем кнопку по ее ID
+
     if (message !== '') {
+        // Запретить отправку сообщений, если isAllowedToSend равно false
+        if (!isAllowedToSend) {
+            console.log("Не разрешено отправлять сообщения.");
+            return;
+        }
+
+        // Отключить кнопку отправки
+        isAllowedToSend = false;
+        sendButton.disabled = true; // Отключить кнопку отправки
+
         drawMessage(senders[0], message, chat_messages_container);
         scrollToBottom(chat_messages_container);
         const combinedData = combineMessageAndSliders(message, nodeSliders);
-        await fetch(endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(combinedData)
-        })
-            .then(async response => await response.json())
-            .then(data => {
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(combinedData)
+            });
+            if (response.ok) {
+                const data = await response.json();
                 console.log(data);
                 if (Array.isArray(data.args) && data.args.length !== 0) {
                     if (func_call_checker.check_availability(data.function_name)) {
@@ -70,11 +85,19 @@ export async function sendMessageAndGetResponse(message, chat_messages_container
                 }
                 drawMessage(senders[1], data.response, chat_messages_container);
                 scrollToBottom(chat_messages_container);
-            })
-            .catch(error => console.log("Ошибка: ", error));
+            } else {
+                console.error("Ошибка в ответе сервера");
+            }
+        } catch (error) {
+            console.error("Ошибка: ", error);
+        } finally {
+            // Включить кнопку отправки после получения ответа или ошибки
+            isAllowedToSend = true;
+            sendButton.disabled = false; // Включить кнопку отправки
+        }
     }
-
 }
+
 
 function getTranslateValue(element) {
     const computedStyle = window.getComputedStyle(element);
