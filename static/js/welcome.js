f291655aaf5fb41ebe2f4020cc7e416d27f90853
svetadomoi <heavensfeel@yandex.ru>
2023-09-08 12:35:48 +0300

    final steps...

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -6,12 +6,14 @@ import { scrollToBottom } from "./chat_functionality.js";
 const welcome_sliders = document.querySelectorAll("input[type='range']");
 const chat_container = document.getElementById("chat-popup");
 const giga_chat = document.getElementById("giga-chat");
+const giga_chat_overlay = document.getElementById("giga-chat-overlay");
 const welcomeMessageContainer = document.getElementById("chat-messages");
 const giga_chat_message_container = document.getElementById("giga-chat-messages");
+const right_container = document.getElementById("right-subcontainer");
 const senders = ["user", "llm"]
 const giga_senders = ["giga-user", "giga-llm"]
 window.cancelWelcome = false;
-const skip_intro = true;
+const skip_intro = false;
 
 export async function sleep(ms) {
     return new Promise(async resolve => await setTimeout(resolve, ms));
@@ -47,11 +49,9 @@ async function welcome_chat(){
       }
     }
   }
-  
-  
 }
 
-giga_chat.classList.toggle("active")
+
 
 window.addEventListener("DOMContentLoaded", async () => {
   if (!skip_intro) {
@@ -59,30 +59,34 @@ window.addEventListener("DOMContentLoaded", async () => {
     const dialog_number = Math.floor(Math.random() * (dialogues.length - 0 + 1)) + 0;
     chat_container.style.bottom = "0px";
     var sender_id = 1;
-    await sleep(2000)
     for (let i = 0; i < dialogues[dialog_number].length; i++) {
-      drawMessage(senders[sender_id%2], dialogues[dialog_number][i], welcomeMessageContainer);
+      await drawMessage(senders[sender_id%2], dialogues[dialog_number][i], welcomeMessageContainer);
       sender_id += 1;
-      scrollToBottom(welcomeMessageContainer);
-      await sleep(1500);
     }
     await fetch("/static/images/hammer.gif")
     .then(async response => await response.blob())
     .then(blob => {
+      const gif_container = document.createElement("div");
+      gif_container.className = "gif-container";
       gif.src = URL.createObjectURL(blob);
-      //document.getElementById("sliders").appendChild(gif);
+      gif_container.appendChild(gif);
+      document.body.appendChild(gif_container);
     });
   
   await sleep(1300)
   chat_container.style.bottom = "-600px";
-  await sleep(500)
+  await sleep(450)
   gif.style.display = "none"
   await sleep(1500)
   }
+
+  giga_chat.classList.toggle("active")
+  giga_chat_overlay.classList.toggle("active")
+  right_container.classList.toggle("active")
   
-  drawMessage("giga-llm", "Простите моего коллегу, он устарел.", giga_chat_message_container);
+  await drawMessage("giga-llm", "Простите моего коллегу, он устарел.", giga_chat_message_container);
   
   await sleep(1500)
-  drawMessage("giga-llm", "Чем могу быть полезен?", giga_chat_message_container);
+  await drawMessage("giga-llm", "Чем могу быть полезен?", giga_chat_message_container);
   await welcome_chat();
 });
